5f9d673a53a5ac601b0a9d4908c63c2b2055ad88
abarrantesc <andres24968@gmail.com>
2022-05-13 16:32:30 -0600

    first commit

---
+++ Project/Controllers/UserController.cs
@@ -0,0 +1,242 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Project.Data.Entities;
+using Project.Enum;
+using Project.Models;
+using Project.Models.BindingModel;
+using Project.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UserController : ControllerBase
+	{
+	
+
+		private readonly ILogger<UserController> _logger;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        private readonly JWTConfig _jWTConfig;
+
+        public UserController(RoleManager<IdentityRole> roleManager , ILogger<UserController> logger, UserManager<AppUser> userManager, SignInManager<AppUser> signManager, IOptions<JWTConfig> jwtConfig)
+        {
+            _userManager = userManager;
+            _signInManager = signManager;
+            _roleManager = roleManager;
+
+            _logger = logger;
+            _jWTConfig = jwtConfig.Value;
+
+        }
+
+        [HttpPost("RegisterUser")]
+        public async Task<object> RegisterUser([FromBody] AddUpdateRegisterUserBindingModel model)
+        {
+            try
+            {
+
+                if(!await _roleManager.RoleExistsAsync(model.Role))
+				{
+                    return await Task.FromResult(new ResponseModel(ResponseCode.Error, "Role does not exist", null));
+
+                }
+
+                var user = new AppUser() { FullName = model.FullName, Email = model.Email, UserName = model.Email, DateCreated = DateTime.UtcNow, DateModified = DateTime.UtcNow };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+
+                    var tempUser = await _userManager.FindByEmailAsync(model.Email);
+                    await _userManager.AddToRoleAsync(tempUser, model.Role);
+
+                    return await Task.FromResult(new ResponseModel(ResponseCode.OK, "User has been Registered", null));
+                }
+                return await Task.FromResult(new ResponseModel(ResponseCode.Error, "", result.Errors.Select(x => x.Description).ToArray()));
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new ResponseModel(ResponseCode.Error, ex.Message, null));
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet("GetAllUser")]
+        public async Task<object> GetAllUser()
+        {
+            try
+            {
+                List<UserDTO> allUserDTO= new();
+
+                var users = _userManager.Users.ToList();
+				foreach (var user in users)
+				{
+
+                    var role = (await _userManager.GetRolesAsync(user)).FirstOrDefault();
+
+                    allUserDTO.Add(new UserDTO(user.FullName, user.Email, user.UserName, user.DateCreated,role));
+
+                  
+                }
+                return await Task.FromResult(new ResponseModel(ResponseCode.OK, "", allUserDTO));
+
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new ResponseModel(ResponseCode.Error, ex.Message, null));
+            }
+        }
+
+        [Authorize(Roles="User,Admin")]
+        [HttpGet("GetUserList")]
+        public async Task<object> GetUserList()
+        {
+            try
+            {
+                List<UserDTO> allUserDTO = new();
+
+                var users = _userManager.Users.ToList();
+                foreach (var user in users)
+                {
+
+                    var role = (await _userManager.GetRolesAsync(user)).FirstOrDefault();
+
+                    allUserDTO.Add(new UserDTO(user.FullName, user.Email, user.UserName, user.DateCreated, role));
+
+
+                }
+                return await Task.FromResult(new ResponseModel(ResponseCode.OK, "", allUserDTO));
+
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new ResponseModel(ResponseCode.Error, ex.Message, null));
+            }
+        }
+
+        [HttpGet("GetRoles")]
+        public async Task<object> GetRoles()
+        {
+            try
+            {
+
+
+                var roles = _roleManager.Roles.Select(x => x.Name).ToList();
+                return await Task.FromResult(new ResponseModel(ResponseCode.OK, "", roles));
+
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new ResponseModel(ResponseCode.Error, ex.Message, null));
+            }
+        }
+
+
+        [HttpPost("Login")]
+        public async Task<object> Login([FromBody] LoginBindingModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+             
+					var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+
+                    if (result.Succeeded)
+                    {
+                        var appUser = await _userManager.FindByEmailAsync(model.Email);
+                        var role = (await _userManager.GetRolesAsync(appUser)).FirstOrDefault();
+
+                        var user = new UserDTO(appUser.FullName, appUser.Email, appUser.UserName, appUser.DateCreated, role)
+                        {
+                            Token = GenerateToken(appUser, role)
+                        };
+                        return await Task.FromResult(new ResponseModel(ResponseCode.OK, "", user));
+
+                    }
+                }
+
+                return await Task.FromResult( new ResponseModel(ResponseCode.Error,"invalid Email or password",null));
+
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new ResponseModel(ResponseCode.Error, ex.Message, null));
+            }
+        }
+
+        [Authorize]
+        [HttpPost("AddRole")]
+        public async Task<object> AddRole([FromBody] AddRoleBindingModel model) 
+        {
+            try
+            {
+               if(model==null || model.Role == null)
+				{
+                    return await Task.FromResult(new ResponseModel(ResponseCode.OK, "parameter are missing", null));
+                }
+
+                if (await _roleManager.RoleExistsAsync(model.Role))
+                {
+                    return await Task.FromResult(new ResponseModel(ResponseCode.OK, "Role already exist", null));
+                }
+
+                var role = new IdentityRole();
+                role.Name = model.Role;
+
+                var result = await _roleManager.CreateAsync(role);
+				if (result.Succeeded)
+				{
+                    return await Task.FromResult(new ResponseModel(ResponseCode.OK, "Role added successfully", null));
+
+                }
+
+                return await Task.FromResult(new ResponseModel(ResponseCode.Error, "Something went wrong, plase try againt later", null));
+
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new ResponseModel(ResponseCode.Error, ex.Message, null));
+            }
+        }
+
+        private string GenerateToken(AppUser user,string role)
+        {
+            var claims = new List<System.Security.Claims.Claim>(){
+     new System.Security.Claims.Claim(JwtRegisteredClaimNames.NameId,user.Id),
+               new System.Security.Claims.Claim(JwtRegisteredClaimNames.Email,user.Email),
+               new System.Security.Claims.Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                new System.Security.Claims.Claim(ClaimTypes.Role,role)
+           };
+            
+
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jWTConfig.Key);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(12),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Audience = _jWTConfig.Audience,
+                Issuer = _jWTConfig.Issuer
+            };
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            return jwtTokenHandler.WriteToken(token);
+        }
+
+
+    }
+}
---
+++ Project/Models/BindingModel/AddUpdateRegisterUserBindingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Models.BindingModel
+{
+
+    public class AddRoleBindingModel
+    {
+        public string Role { get; set; }
+    }
+}
